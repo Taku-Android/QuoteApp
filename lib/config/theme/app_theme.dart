0d94f81ebb589f417f714744dafbfbd504df4d44
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2023-08-11 18:16:34 +0300

    handling app_fonts

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -1,6 +1,5 @@
-
-
 import 'package:clean_arch/core/utils/app_color.dart';
+import 'package:clean_arch/core/utils/app_strings.dart';
 import 'package:flutter/material.dart';
 
 ThemeData appTheme() {
@@ -8,7 +7,7 @@ ThemeData appTheme() {
     primaryColor: AppColor.primaryColor ,
     hintColor: AppColor.hint ,
     brightness: Brightness.light ,
-
+    fontFamily: AppStrings.fontFamily,
     textTheme: const TextTheme(
       labelLarge: TextStyle(
         fontSize: 16 ,
