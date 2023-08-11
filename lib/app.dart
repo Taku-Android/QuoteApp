758c7b72924394c5d43026a8ccd88bfa938e5fb1
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2023-08-11 14:55:24 +0300

    remove debug par adding text

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,4 @@
+import 'package:clean_arch/features/randomquotes/presentation/screens/quote_app.dart';
 import 'package:flutter/material.dart';
 
 class QuoteApp extends StatelessWidget {
@@ -6,11 +7,12 @@ class QuoteApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Quotes App',
       theme: ThemeData(
         primarySwatch: Colors.blue
       ),
-      home: const QuoteApp(),
+      home: const QuoteScreen(),
     );
   }
 }
--- lib/features/randomquotes/presentation/screens/quote_app.dart
+++ lib/features/randomquotes/presentation/screens/quote_app.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 
-class QuoteApp extends StatelessWidget {
-  const QuoteApp({super.key});
+class QuoteScreen extends StatelessWidget {
+  const QuoteScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'Quotes' ,
+          style: TextStyle(
+            fontSize: 24 ,
+            color: Colors.black ,
+          ),
+        ),
+      ),
+    );
   }
 }
