3ea25208b040104b4f245680dcadb729afd5a3d5
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2023-08-11 15:02:49 +0300

    adding the app name to utils dir in core dir
    in app_strings.dart class
      - it will hold all app strings for 2 reason
           1 - it will hold all the strings i will use alot time
           2 - if i want localization to my app all strings i can replace

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,8 @@
 import 'package:clean_arch/features/randomquotes/presentation/screens/quote_app.dart';
 import 'package:flutter/material.dart';
 
+import 'core/utils/app_strings.dart';
+
 class QuoteApp extends StatelessWidget {
   const QuoteApp({super.key});
 
@@ -8,7 +10,7 @@ class QuoteApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Quotes App',
+      title: AppStrings.appName,
       theme: ThemeData(
         primarySwatch: Colors.blue
       ),
