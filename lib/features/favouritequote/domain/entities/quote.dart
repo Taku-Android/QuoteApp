292c084525c4c0528edfc4fcba106e9cd5f04922
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2023-08-12 21:34:42 +0300

    adding entities for model using equatable

---
+++ lib/features/favouritequote/domain/entities/quote.dart
@@ -0,0 +1,17 @@
+import 'package:equatable/equatable.dart';
+
+class Quote extends Equatable{
+
+  final String author ;
+  final String content ;
+  final String permalink ;
+  final int id ;
+
+  const Quote({required this.author,required this.content,required this.permalink,required this.id});
+
+  @override
+  List<Object?> get props => throw UnimplementedError();
+
+
+
+}
